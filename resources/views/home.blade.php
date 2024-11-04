e882289890cfa9759cb632a8afd2b62d2679c810
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-04 15:54:13 +0100

    Set up home.blade.php as dashboard and updated root route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.app')
 
+@section('title', 'Dashboard')
+
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+    <div class="row">
+        <div class="col-md-12 text-center my-4">
+            <h1>Benvenuto nella tua Dashboard del Portfolio</h1>
+            <p>Qui puoi gestire i tuoi progetti, visualizzare le statistiche e molto altro.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-header">Progetti Recenti</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <p>Nessun progetto recente.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-header">Statistiche</div>
+                <div class="card-body">
+                    <p>Nessun dato disponibile.</p>
                 </div>
             </div>
         </div>
