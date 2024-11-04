2ef70ef7e85ede515d4f87b964432c8805a8d6ea
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-04 15:42:31 +0100

    Added header and footer partials to main layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-light text-center text-lg-start mt-5">
+    <div class="text-center p-3">
+        © 2024 Laravel-Auth | Francesco Matteucci
+        <br>
+        <a href="https://github.com/Francesco-Matteucci" target="_blank">GitHub</a>
+    </div>
+</footer>
