103f43e2d8f2fa59aa8aef1983a521345d6cfd0f
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-11-04 16:46:16 +0100

    Created Admin ProjectController and configured CRUD routes for project management

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
+    Route::get('/projects', 'ProjectController@index')->name('projects.index');
+    Route::get('/projects/create', 'ProjectController@create')->name('projects.create');
+    Route::post('/projects', 'ProjectController@store')->name('projects.store');
+    Route::get('/projects/{project}', 'ProjectController@show')->name('projects.show');
+    Route::get('/projects/{project}/edit', 'ProjectController@edit')->name('projects.edit');
+    Route::put('/projects/{project}', 'ProjectController@update')->name('projects.update');
+    Route::delete('/projects/{project}', 'ProjectController@destroy')->name('projects.destroy');
+});
